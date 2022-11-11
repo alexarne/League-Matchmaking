3e072505df24816b8526c86d541993f35ea16a4a
Alex Gunnarsson <alexgunnarsson@live.se>
2022-11-11 15:49:08 +0100

    test server

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-// import fetch from "node-fetch"
+const http = require("http")
+const port = 3000
 
-const API_KEY = process.env.API_KEY
-console.log(API_KEY)
-// fetch("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY)
-//     .then(response => response.text())
-//     .then(data => document.getElementById("content").innerHTML = data)
-//     .catch(err => document.getElementById("content").innerHTML = err)
+const server = http.createServer(function(req, res) {
+    res.write("Hello Node")
+    res.end()
+})
+
+server.listen(port, function(error) {
+    if (error) {
+        console.log("Something went wrong", error)
+    } else {
+        console.log("Server listening on port " + port)
+    }
+})
 
 
-console.log("print");
+// // import fetch from "node-fetch"
+
+// const API_KEY = process.env.API_KEY
+// console.log(API_KEY)
+// // fetch("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY)
+// //     .then(response => response.text())
+// //     .then(data => document.getElementById("content").innerHTML = data)
+// //     .catch(err => document.getElementById("content").innerHTML = err)
+
+
+// console.log("print");
