b3b8b11528c651f94bd78823b963a9fae22f3dc0
Alex Gunnarsson <alexgunnarsson@live.se>
2022-11-11 16:51:05 +0100

    display html

--- index.js
+++ index.js
@@ -2,20 +2,32 @@
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const http = require("http")
+const fs = require("fs")
 require("dotenv").config()
 const port = process.env.PORT || 3000
 
 
 const server = http.createServer(async function(req, res) {
-    res.write("Hello Node")
+    res.writeHead(200, { "Content-Type": "text/html" })
+    fs.readFile("index.html", function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write("Error: File Not Found")
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
 
-    const API_KEY = process.env.API_KEY
+
+    // res.write("Hello Node")
+
+    // const API_KEY = process.env.API_KEY
     
-    const response = await fetch("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY)
-    const data = await response.text()
-    res.write(data)
+    // const response = await fetch("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY)
+    // const data = await response.text()
+    // res.write(data)
     
-    res.end()
 })
 
 server.listen(port, function(error) {
