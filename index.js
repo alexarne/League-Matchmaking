628304e61a5b9fd9b882217399632cc8062bde13
Alex Gunnarsson <alexgunnarsson@live.se>
2022-11-11 12:18:55 +0100

    idk

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // import fetch from "node-fetch"
 
 const API_KEY = process.env.API_KEY
-
-fetch("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY)
-    .then(response => response.text())
-    .then(data => document.getElementById("content").innerHTML = data)
-    .catch(err => document.getElementById("content").innerHTML = err)
+console.log(API_KEY)
+// fetch("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY)
+//     .then(response => response.text())
+//     .then(data => document.getElementById("content").innerHTML = data)
+//     .catch(err => document.getElementById("content").innerHTML = err)
 
 
 console.log("print");
