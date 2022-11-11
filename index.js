d023735a53100a40aded2296d2b02cc4fda6d7f3
Alex Gunnarsson <alexgunnarsson@live.se>
2022-11-11 16:16:53 +0100

    print api return

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const port = process.env.PORT || 3000
 
 const server = http.createServer(function(req, res) {
     res.write("Hello Node")
-    res.write("Hello Node")
+    res.write("Hello Node:")
+
+    const API_KEY = process.env.API_KEY
+    console.log(API_KEY)
+    fetch("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY)
+        .then(response => response.text())
+        .then(data => res.write(data))
+    
     res.end()
 })
 
@@ -20,10 +27,10 @@ server.listen(port, function(error) {
 
 // const API_KEY = process.env.API_KEY
 // console.log(API_KEY)
-// // fetch("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY)
-// //     .then(response => response.text())
-// //     .then(data => document.getElementById("content").innerHTML = data)
-// //     .catch(err => document.getElementById("content").innerHTML = err)
+// fetch("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + API_KEY)
+//     .then(response => response.text())
+//     .then(data => document.getElementById("content").innerHTML = data)
+//     .catch(err => document.getElementById("content").innerHTML = err)
 
 
 // console.log("print");
